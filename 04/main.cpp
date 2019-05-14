b3183e90b46efd8f31f6f8ea58388581ac4a5db1
u211-18 <u211-18@public.mpei.local>
2019-05-14 17:29:01 +0300

    sdf

--- 04/main.cpp
+++ 04/main.cpp
@@ -1,109 +1,8 @@
 #include <iostream>
 #include <cassert>
+#include "byteprint.h"
 
 using namespace std;
-
-char nibble_to_hex (uint8_t i)
-{
-    char symbols[17]="0123456789abcdef";
-    return symbols[i];
-}
-
-char
-bit_digit(uint8_t byte, uint8_t bit) {
-    if (byte & (0x1 << bit)) {
-        return '1';
-    }
-    return '0';
-}
-
-void
-print_in_binary(uint8_t byte) {
-    for (uint8_t bit = 7; bit >= 0; bit--) {
-
-        cout << bit_digit(byte, bit);
-      if (bit==0)
-            return;
-    }
-}
-
-const uint8_t*
-as_bytes(const void* data)
-{
-    return reinterpret_cast<const uint8_t*>(data);
-}
-
-
-void
-print_in_binary(const void* data, size_t size) {
-    const uint8_t* bytes = as_bytes(data);
-    for (size_t i = 0; i < size; i++) {
-        print_in_binary(bytes[i]);
-        if ((i + 1) % 4 == 0) {
-            cout << '\n';
-        }
-        else {
-            cout << ' ';
-        }
-    }
-}
-
-
-void print_in_hex(uint8_t byte)
-{
-    cout << nibble_to_hex(byte>>4) << nibble_to_hex(byte&0x0f);
-}
-
-
-void
-print_in_hex(const void* data, size_t size)
-{
-    const uint8_t* bytes = as_bytes(data);
-    for (size_t i = 0; i < size; i++)
-    {
-        print_in_hex(bytes[i]);
-
-
-        if ((i + 1) % 16 == 0)
-        {
-            cout << '\n';
-        }
-        else
-        {
-            cout << ' ';
-        }
-    }
-}
-
-void calc (uint16_t op1, char operat ,uint16_t op2)
-{
-    print_in_hex(&op1,sizeof(op1));
-    cout << ' ' << operat << ' ';
-     print_in_hex(&op2,sizeof(op2));
-     cout << " = ";
-    uint16_t res;
-    if (operat == '&')
-    res=op1 & op2;
-    if (operat == '|')
-    res=op1 | op2;
-    if (operat == '^')
-    res=op1 ^ op2;
-     print_in_hex(&res,sizeof(res));
-    cout << endl;
-     print_in_binary(&op1,sizeof(op1));
-    cout << ' ' << operat << ' ';
-    print_in_binary(&op2,sizeof(op2));
-    cout << " = ";
-    print_in_binary(&res,sizeof(res));
-
-
-
-
-}
-
-void print_in_binary(uint8_t byte);
-void print_in_binary(const void* data, size_t size);
-
 
 struct student {
 char name [17];
@@ -171,7 +70,7 @@ int main()
     cout << endl << "print_in_hex of student 0 average  "; print_in_hex(&students[0].average,sizeof(students[0].average));
     cout << endl << "print_in_binary of student 0 average  "; print_in_binary(&students[0].average,sizeof(students[0].average));
     cout << endl;
-    cout << endl << "address of student 0 courses " << &students[0].courses;
+    cout << endl << "address of student 0 courses " << (void*)&students[0].courses;
     cout << endl << "offset of student 0 courses " <<offsetof(student, courses);
     cout << endl << "Size of student 0 courses  " << sizeof(students[0].courses);
     cout << endl << "print_in_hex of student 0 courses  "; print_in_hex(&students[0].courses,sizeof(students[0].courses));
@@ -182,6 +81,7 @@ int main()
     cout << endl << "Size of student 0 starosta  " << sizeof(students[0].starosta);
     cout << endl << "print_in_hex of student 0 starosta  "; print_in_hex(&students[0].starosta,sizeof(students[0].starosta));
     cout << endl << "print_in_binary of student 0 starosta  "; print_in_binary(&students[0].starosta,sizeof(students[0].starosta));
+    cout << endl;      cout << endl;
+    cout << endl << "print_in_hex of student 0 courses  "; print_in_hex(&students,sizeof(students));
 
 }
-
